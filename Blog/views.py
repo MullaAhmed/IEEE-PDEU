452f26a7b3e5bd90710eba6fce059840986296f7
Ahmed Mulla <ahmedmulla901@gmail.com>
2021-08-02 23:58:06 +0530

    Initial Commit

---
+++ Blog/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def blog_home(request):
+    return render(request,"Blog/blog_home.html")
+def blog_page(request):
+    return render(request,"Blog/page.html")
---
+++ website/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+# Create your views here.
+def base(response):
+    return render(response,"website/base.html")
+def home(response):
+    return render(response,"website/home.html")
+def events(response):
+    return render(response,"website/event.html")
+def about(response):
+    return render(response,"website/about.html")
+def contact(response):
+    return render(response,"website/contact.html")

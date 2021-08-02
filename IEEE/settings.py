b9c7a35acce3352112d056759f3cfd52fbda7c3e
Ahmed Mulla <ahmedmulla901@gmail.com>
2021-08-03 00:14:57 +0530

    Changed Settings.py

--- IEEE/settings.py
+++ IEEE/settings.py
@@ -122,10 +122,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT=os.path.join(BASE_DIR,'static')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, '/Blog/static/'),
-    
-]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field

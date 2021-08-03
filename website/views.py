fc277beb578486f685926d14f60b16b67c151132
MullaAhmed <67974888+MullaAhmed@users.noreply.github.com>
2021-08-03 10:13:30 +0530

    Update views.py

--- website/views.py
+++ website/views.py
@@ -2,12 +2,12 @@ from django.shortcuts import render
 
 # Create your views here.
 def base(response):
-    return render(response,"/website/base.html")
+    return render(response,"website/base.html")
 def home(response):
-    return render(response,"/website/home.html")
+    return render(response,"website/home.html")
 def events(response):
-    return render(response,"/website/event.html")
+    return render(response,"website/event.html")
 def about(response):
-    return render(response,"/website/about.html")
+    return render(response,"website/about.html")
 def contact(response):
-    return render(response,"/website/contact.html")
+    return render(response,"website/contact.html")

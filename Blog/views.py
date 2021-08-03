b8f1400775a47fd58a37d970e7f5917230f33b06
MullaAhmed <67974888+MullaAhmed@users.noreply.github.com>
2021-08-03 10:13:04 +0530

    Update views.py

--- Blog/views.py
+++ Blog/views.py
@@ -3,6 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 
 def blog_home(request):
-    return render(request,"/Blog/blog_home.html")
+    return render(request,"Blog/blog_home.html")
 def blog_page(request):
-    return render(request,"/Blog/page.html")
+    return render(request,"Blog/page.html")

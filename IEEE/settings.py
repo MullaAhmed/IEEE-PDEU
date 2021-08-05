300d300c73a28d2c5deba6c85070cc7c3a6b3fdf
MullaAhmed <67974888+MullaAhmed@users.noreply.github.com>
2021-08-05 21:40:01 +0530

    Update settings.py

--- IEEE/settings.py
+++ IEEE/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/IEEE/static/'
+STATIC_URL = '/static/'
 STATIC_ROOT=os.path.join(BASE_DIR,'IEEE\\static')
 STATICFILES_DIRS = ['../IEEE/static/']
 

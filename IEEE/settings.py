8470038ef7754193a62424a4ba022d865e6ab2db
bmg02 <bhuvan12501@gmail.com>
2021-08-05 23:10:58 +0530

    Updated static path

--- IEEE/settings.py
+++ IEEE/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'website.apps.WebsiteConfig',
-    'Blog.apps.BlogConfig',
+    'blog.apps.BlogConfig',
 ]
 
 MIDDLEWARE = [

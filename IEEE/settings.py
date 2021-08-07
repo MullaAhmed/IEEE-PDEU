48a46a48ef70bed58c0d6c2f00fdd8e9e066fd58
MullaAhmed <67974888+MullaAhmed@users.noreply.github.com>
2021-08-07 23:31:49 +0530

    Update settings.py

--- IEEE/settings.py
+++ IEEE/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-dus&xo(ogbabo&!!$(cnr3s8j*+)m&5$umc^*d^j37e90g!#)d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["haus-connect.herokuapp.com/",'127.0.0.1']
 
